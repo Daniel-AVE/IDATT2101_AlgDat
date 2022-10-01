432fdcacf8f26c9afd0a8f913c9d268d93940cb3
Daniel Evensen <daevense@stud.ntnu.no>
2022-10-01 20:10:51 +0200

    worked on HashTable file, and implemented a lot of new methods

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -1,61 +1,182 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
 public class Hashtable {
+    private LinkedList hashTable;
+    private int numberOfCollisions;
+    private LinkedList[] nodes;
+    private String collisions;
 
-    /*public int insert(int key, int[] hashtable) {
-        int m = hashtable.length;
-        int h = divHash(key, m);
-        for (int i = 0; i < m; i++) {
-            int position = probe
+    public Hashtable() {
+        hashTable = new LinkedList();
+        numberOfCollisions = 0;
+        nodes = new LinkedList[145];
+        collisions = "";
+    }
+
+    public int hash(String element) {
+        int hash = 0;
+        for (int i = 0; i < element.length(); i++) {
+            hash += (13 * element.charAt(i)) % nodes.length;
         }
-    }*/
+        return hash;
+    }
 
-    // Function to print an array
-    static void printArray(int arr[])
-    {
+    public int divHash(int k, int m) {
+        return k % m;
+    }
 
-        // Iterating and printing the array
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public void insert(String element) {
+        int hashed = hash(element);
+
+        if (nodes[divHash(hashed, nodes.length)] == null) {
+            nodes[divHash(hashed, nodes.length)] = new LinkedList();
+            nodes[divHash(hashed, nodes.length)].insertAtBack(element, hashed);
+        } else {
+            collisions += nodes[divHash(hashed, nodes.length)].getTail().getElementAndkey() + " collides with: ";
+            nodes[divHash(hashed, nodes.length)].insertAtBack(element, hashed);
+            collisions += nodes[divHash(hashed, nodes.length)].getTail().getElementAndkey() + "\n";
+            numberOfCollisions++;
         }
     }
 
-    // Function to implement the
-    // quadratic probing
-    static void hashing(int table[], int tsize, int arr[],
-                        int N)
-    {
+    public int getNumberOfCollisions() {
+        return numberOfCollisions;
+    }
 
-        // Iterating through the array
-        for (int i = 0; i < N; i++) {
+    public String getCollisions() {
+        return collisions;
+    }
 
-            // Computing the hash value
-            int hv = arr[i] % tsize;
+    public double getCollisionsPerPerson() {
+        return (double) numberOfCollisions / nodes.length;
+    }
 
-            // Insert in the table if there
-            // is no collision
-            if (table[hv] == -1)
-                table[hv] = arr[i];
-            else {
+    public int getTotElements() {
+        int totElements = 0;
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] == null) ;
+            else totElements = nodes[i].getTotElements();
+        }
+        return totElements;
+    }
 
-                // If there is a collision
-                // iterating through all
-                // possible quadratic values
-                for (int j = 0; j < tsize; j++) {
+    public HashNode findPerson(String nameOfPerson) {
+        System.out.println(nodes[divHash(hash(nameOfPerson), nodes.length)]);
+        return nodes[divHash(hash(nameOfPerson), nodes.length)].findPerson(nameOfPerson);
+    }
 
-                    // Computing the new hash value
-                    int t = (hv + j * j) % tsize;
-                    if (table[t] == -1) {
+    public double getLoad() {
+        return (double) getTotElements() / nodes.length;
+    }
 
-                        // Break the loop after
-                        // inserting the value
-                        // in the table
-                        table[t] = arr[i];
-                        break;
-                    }
+    public static void main(String[] args) throws IOException {
+        Hashtable hashTable = new Hashtable();
+        String[] names = new String[114];
+
+        try {
+            BufferedReader b = new BufferedReader(new FileReader(new File("src/navn.txt")));
+            Scanner sc = new Scanner(b);
+            int num = 0;
+            if (!sc.hasNext()) {
+                System.out.println("File is empty");
+            } else {
+                while (sc.hasNext()) {
+                    String data = sc.nextLine();
+                    names[num] = data;
+                    num++;
                 }
+                sc.close();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        printArray(table);
+        for (int i = 0; i < names.length; i++) {
+            hashTable.insert(names[i]);
+        }
+
+        System.out.println(hashTable.getCollisions());
+        System.out.println("Number of collisions: " + hashTable.getNumberOfCollisions() + "\nCollisions per person: " + hashTable.getCollisionsPerPerson() + "\nLoad: " + hashTable.getLoad());
+
+        //Slår opp på person
+        System.out.println("Find me: " + hashTable.findPerson("Daniel André Vestly Evensen"));
+    }
+
+    class HashNode {
+        String element;
+        HashNode next;
+        int key;
+
+        public HashNode(String element, HashNode next, int key) {
+            this.key = key;
+            this.element = element;
+            this.next = next;
+        }
+
+        public String getElement() {
+            return element;
+        }
+
+
+        public String getElementAndkey() {
+            return element + ", " + key;
+        }
+
+        @Override
+        public String toString() {
+            return "Node {" +
+                    " element = '" + element + '\'' +
+                    ", neste = " + next +
+                    ", key = " + key +
+                    '}';
+        }
+    }
+
+    class LinkedList {
+        private HashNode head, tail = null;
+        private int totElements = 0;
+
+        public void insertAtBack(String element, int key) {
+            HashNode node = new HashNode(element, null, key);
+
+            if (head == null) head = node;
+            else tail.next = node;
+
+            tail = node;
+            tail.next = null;
+            totElements++;
+        }
+
+        public HashNode findPerson(String nameOfPerson) {
+            HashNode node = head;
+
+            for (int i = 0; i < totElements; i++) {
+                if (node.getElement().equals(nameOfPerson)) return node;
+                node = node.next;
+            }
+            return null;
+        }
+
+        public int getTotElements() {
+            return totElements;
+        }
+
+        public HashNode getHead() {
+            return head;
+        }
+
+        public HashNode getTail() {
+            return tail;
+        }
+
+        @Override
+        public String toString() {
+            return "LinkedList {" +
+                    " head = " + head.toString() +
+                    ", totElements = " + totElements +
+                    "}";
+        }
     }
 }
